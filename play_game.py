b16d91496eac451f13738830dd11e5ef15beb1de
maxvelasco <50286542+maxvelasco@users.noreply.github.com>
2023-12-18 21:21:09 -0500

    finalized testing

--- play_game.py
+++ play_game.py
@@ -63,37 +63,14 @@ if __name__ == "__main__":
     
     agents = [always_bet_agent, always_fold_agent, random, dqn, cfr, ev_30, ev_50, ev_70, ev_80, ev_90]
 
-    #P0 is the first agent, P1 is the second agent. We will exchange which agent bets first in the simulations
-    
-    for p0 in agents:
-        for p1 in agents:
 
-            if p0 != p1:
-                play_matchup(p0, p1)
+    print("DQN vs. all agents")
+    for p1 in agents:
+
+        play_matchup(dqn, p1)
 
 
+    print("\nCFR vs. all agents")    
+    for p1 in agents:
 
-    # p0_wins = 0
-    # p0_reward = 0
-    # ties = 0
-
-    # #Simulate game
-    # for i in range(num_games):
-    #     #NOTE: since there is an advantage to being P1, when evaluating 2 agents, we should alternate between which policy is P0 and P1 (equal number)
-    #     # To simulate the agents swapping positions
-    #     if i % 2 == 0:
-    #         winner, margin = game.play(p0_agent, p1_agent, verbose=False) #Set Verbose to True if you want to see how the game plays out
-    #         if winner == 1:
-    #             p0_wins += 1 #Since p0 was put as p0 in the simulation
-    #         elif winner == 0:
-    #             ties += 1
-    #         p0_reward += margin
-    #     else:
-    #         winner, margin = game.play(p1_agent, p0_agent, verbose=False)
-    #         if winner == -1:
-    #             p0_wins += 1 #Since p0 was put as p1 in the simulation
-    #         elif winner == 0:
-    #             ties += 1
-    #         p0_reward -= margin
-    
-    # print(f"{p0_agent} vs {p1_agent}\n{num_games} games.\nP0 avg reward/game: {(p0_reward/num_games):.2f}\nP0 win: {(p0_wins/num_games) * 100:.2f}%. Tie: {(ties/num_games) * 100:.2f}%. P1 win: {((num_games-ties-p0_wins)/num_games) * 100:.2f}%")
+        play_matchup(cfr, p1)
